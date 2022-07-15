fc3a67429ca6a7a60744dc53864296c06f9c663b
iimagii <mag_1@ukr.net>
2022-07-15 12:01:51 +0300

    add navigattion and add redux

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,17 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import { FavoritesPage } from './pages/FavoritesPage';
 import { HomePage } from './pages/HomePage';
+import { Navigations } from './components/Navigations';
 
 function App() {
    return (
-      <Routes>
-         <Route path='/' element={<HomePage />} />
-         <Route path='/favorites' element={<FavoritesPage />} />
-      </Routes>
+      <>
+         <Navigations />
+         <Routes>
+            <Route path='/' element={<HomePage />} />
+            <Route path='/favorites' element={<FavoritesPage />} />
+         </Routes>
+      </>
    );
 }
 
