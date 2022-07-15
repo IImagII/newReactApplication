8089f273a41d464ce239b5ba7c28e53b04036c96
iimagii <mag_1@ukr.net>
2022-07-15 22:06:14 +0300

    made home page

---
+++ src/hooks/debounce.ts
@@ -0,0 +1,10 @@
+import { useEffect, useState } from 'react';
+
+export function useDebounce(value: string, delay = 300): string {
+   const [debounce, setDebounced] = useState(value);
+   useEffect(() => {
+      const handler = setTimeout(() => setDebounced(value), delay);
+      return () => clearTimeout(handler);
+   }, [value, delay]);
+   return debounce;
+}
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,9 +1,22 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useDebounce } from '../hooks/debounce';
 import { useSearchUsersQuery } from '../store/github/github.api';
 
 export function HomePage() {
-   const { isLoading, isError, data } = useSearchUsersQuery('IImagII');
-   console.log(data);
+   const [search, setSearch] = useState('');
+   const [dropdown, setDropdown] = useState(false);
+
+   const debounce = useDebounce(search);
+
+   const { isLoading, isError, data } = useSearchUsersQuery(debounce, {
+      skip: debounce.length < 3,
+      refetchOnFocus: true,
+   });
+
+   useEffect(() => {
+      setDropdown(debounce.length > 3 && data?.length! > 0);
+   }, [debounce, data]);
+
    return (
       <div className='flex justify-center pt-10 mx-auto h-screen w-screen'>
          {isError && (
@@ -14,11 +27,22 @@ export function HomePage() {
                type='text'
                className='border py-2 px-4 w-full h-[42px] mb-2'
                placeholder='Search for Gitub username...'
+               value={search}
+               onChange={(e) => setSearch(e.target.value)}
             />
-            <div className='absolute top-[42px] left-0 right-0 max-h-[200px] shadow-md bg-white'>
-               Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-               Dignissimos, sit.
-            </div>
+            {debounce && (
+               <ul className='list-none absolute top-[42px] overflow-y-scroll left-0 right-0 max-h-[200px] shadow-md bg-white p-[10px]'>
+                  {isLoading && <p className='text-center'>Loading...</p>}
+                  {data?.map((user) => (
+                     <li
+                        key={user.id}
+                        className='py-2 px-4 hover:bg-gray-500 hover:text-white transition-colors cursor-pointer'
+                     >
+                        {user.login}
+                     </li>
+                  ))}
+               </ul>
+            )}
          </div>
       </div>
    );
