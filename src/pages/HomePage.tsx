3c8d93f086894e5a89732f9f454ae9b6c8669a87
iimagii <mag_1@ukr.net>
2022-07-15 15:04:34 +0300

    add function on the page Homepage

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,5 +1,25 @@
 import React from 'react';
+import { useSearchUsersQuery } from '../store/github/github.api';
 
 export function HomePage() {
-   return <div>Home</div>;
+   const { isLoading, isError, data } = useSearchUsersQuery('IImagII');
+   console.log(data);
+   return (
+      <div className='flex justify-center pt-10 mx-auto h-screen w-screen'>
+         {isError && (
+            <p className='text-center text-red-600 '>Something went wrong...</p>
+         )}
+         <div className='relative w-[560px]'>
+            <input
+               type='text'
+               className='border py-2 px-4 w-full h-[42px] mb-2'
+               placeholder='Search for Gitub username...'
+            />
+            <div className='absolute top-[42px] left-0 right-0 max-h-[200px] shadow-md bg-white'>
+               Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+               Dignissimos, sit.
+            </div>
+         </div>
+      </div>
+   );
 }
